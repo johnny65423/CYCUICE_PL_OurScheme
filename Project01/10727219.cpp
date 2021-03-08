a5efcc9159413f50625d62502110adec9a7d2444
johnny65423 <johnny65423@gmail.com>
2021-03-08 13:09:39 +0800

    .

--- Project01/10727219.cpp
+++ Project01/10727219.cpp
@@ -7,22 +7,55 @@ using namespace std;
 
 enum Type { LPAREN, PAREN, INT, STRING, DOT, FLOAT, NIL, T, QUOTE, SYMBOL };
 
-class Token{
+int gLine = 1 ;              // 「下一個要讀進來的字元」所在的line number
+int gColumn = 1 ;            // 「下一個要讀進來的字元」所在的column number
+
+struct Token {
+  string str ;
+  int line ;
+  int column ;
+  Type type ;
+};
+
+class Scanner{
   public:
-  void PrintV() {
-      
-    cout << "vvv" ;
+  void Print() {
+    string str = "aaa" ;
+    str+= Getchar() ;
+    cout << str ;
   } // PrintV()
     
   private:
   void PrintA() {
-      
+    
     cout << "a" ;
   } // PrintA()
+  
+  char Getchar(){
+    char ch = '\0' ;
+    
+    scanf( "%c", &ch ) ;
+    gColumn++ ;
+    if ( ch == '\n' ) {
+      gColumn = 1 ;
+      gLine++ ;
+    }
+    return ch ;
+  
+  }
+  
+  void Gettokenlist() {
+    string temp = "" ;
+    temp = Gettoken() ;
+    while( temp != "exit" ) {
+      
+    }
+    
+  }
   
 };
 
 int main() {
-  Token t = Token() ;
-  t.PrintV() ;
+  Scanner scanner = Scanner() ;
+  scanner.Print() ;
 } // main()
