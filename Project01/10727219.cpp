5dc1a02ec9f08e7acc39e9a5fb2ea885da3745da
johnny65423 <johnny65423@gmail.com>
2021-03-08 22:38:56 +0800

    .

--- Project01/10727219.cpp
+++ Project01/10727219.cpp
@@ -17,22 +17,61 @@ struct Token {
   Type type ;
 };
 
+bool iswhitespace( char ch ) {
+  if ( ch == ' ' ) return true ;
+  else if ( ch == '\t' ) return true ;
+  else if ( ch == '\n' ) return true ;
+  else return false ;
+  
+}
+
+bool isdight( char ch ) {
+  if ( ch >= '0' && ch <= '9' ) return true ;
+  else return false ;
+		  
+}
+
+bool isseparators( char ch ) {
+  if ( ch == '(' || ch == ')' ) return true ;
+  else if ( ch == ';' || ch == '\"'  || ch == '\'' ) return true ;
+  else if ( iswhitespace( ch ) ) return true ;
+  else return false ;
+		  
+}
+
+
 class Scanner{
   public:
-  void Print() {
-    string str = "aaa" ;
-    str+= Getchar() ;
-    cout << str ;
-  } // PrintV()
+  char ch ;  
+  
+  Scanner() {
+    ch = '\0' ;
+    
+  }
+  void Readnwschar(){
+    while( ch == ' ' || ch == '\n' || ch == '\t' || ch == '\0' ) {
+      Getchar() ;
+    }
+  }
+  
+  void Gettokenlist() {
+    
+    Readnwschar() ;
+    string temp = "" ;
+    temp = Gettoken() ;
+    while( temp != "\"exit\"" ) {
+      
+      cout << "Token  "<< temp << endl ;
+      
+      temp = Gettoken() ;
+      
+    }
+    cout << "done" ;
+  }
     
   private:
-  void PrintA() {
-    
-    cout << "a" ;
-  } // PrintA()
-  
-  char Getchar(){
-    char ch = '\0' ;
+
+  void Getchar(){
     
     scanf( "%c", &ch ) ;
     gColumn++ ;
@@ -40,22 +79,54 @@ class Scanner{
       gColumn = 1 ;
       gLine++ ;
     }
-    return ch ;
   
   }
   
-  void Gettokenlist() {
-    string temp = "" ;
-    temp = Gettoken() ;
-    while( temp != "exit" ) {
+  string Gettoken(){
+    Readnwschar() ;
+    
+    if( ch == '\"' ) {
       
+      return Getstring( ch ) ;
+    }
+    else if ( isseparators(ch) ) {
+      return Getseparators( ch ) ;
+    }
+  }
+  
+  string Getstring( char & ch ) {
+    string temp = "" ;
+    temp += ch ;
+    Getchar() ;
+       
+    while ( ch != '\"' ){
+      temp += ch ;
+      
+      if( ch == '\\' ) {
+        Getchar() ; 
+        temp += ch ;
+      }
+      
+      Getchar() ; 
     }
     
-  }
+    temp += ch ;
+    Getchar() ;
+    
+    return temp ;
+  } 
+
+  string Getseparators( char & ch ) {
+    string temp = "" ; 
+    temp += ch ;
+    Getchar() ;
+    
+    return temp ;
+  } 
   
 };
 
 int main() {
   Scanner scanner = Scanner() ;
-  scanner.Print() ;
+  scanner.Gettokenlist() ;
 } // main()
