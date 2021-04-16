1af338a347da1b8cfc7a4d3218c2211995229d42
johnny65423 <53753859+johnny65423@users.noreply.github.com>
2021-04-17 02:50:45 +0800

    .

--- OS/10727219.py
+++ OS/10727219.py
@@ -1,21 +1,24 @@
 import time
 import threading
+from numba import jit
+@jit()
 
 
 
-def bubblesort(data):
+def Bubblesort(data, num):
     # 定義資料長度
     n = len(data)
+    xxx = n / 100
+    int = 1
     for i in range(n-2):                   # 有 n 個資料長度，但只要執行 n-1 次
+        #if( i % xxx == 0 ):
+        #    print( int,"%" )
+        #    int+= 1
         for j in range(n-i-1):             # 從第1個開始比較直到最後一個還沒到最終位置的數字 
             if data[j] > data[j+1]:        # 比大小然後互換
                 data[j], data[j+1] = data[j+1], data[j]
-
-
-
-
-
-
+    
+    print(num,"done")
 
 
 #filename = input("請輸入檔案名稱：")
@@ -35,17 +38,21 @@ while num != '':
     num = f.readline()
 f.close()
 
-print(len(numlist))
+#====================================Mission1====================================
+
+m1list = numlist.copy()
 
 start = time.time()
 
-bubblesort(numlist)
+Bubblesort(m1list,0)
 
 end = time.time()
 
-print("執行時間：%f 秒" % (end - start))
+print("M1執行時間：%f 秒" % (end - start))
 
-newlist=[]
+#====================================Mission1====================================
+
+#====================================Mission2====================================
 
 step = int(len(numlist) / k)+1
 
@@ -54,29 +61,38 @@ if len(numlist) % k == 0 :
     step -= 1
 else:
     add = len(numlist) % k
-print("len:",step)
-
-print( len(numlist) )
+    
+start = time.time()    
+    
 index = 0
+m2list=[]
 while ( index < len(numlist) ) :
-    #print(index)
-    #newlist.append(numlist[index: index + step])
+
     if( add > 0 ):
-        #print(numlist[index: index + step])
-        newlist.append(numlist[index: index + step])
+        m2list.append(numlist[index: index + step])
         add-=1
         
         
     else:
-        #print(numlist[index: index + step])
-        newlist.append(numlist[index: index + step])
+        m2list.append(numlist[index: index + step])
     index = index + step 
     if( add == 0 ):
         add = -1
         step -= 1  
         
-len(newlist)        
-for i in range(len(newlist)):
-    print( len(newlist[i]) )
+m2thread = []    
+for i in range(len(m2list)):
+    m2thread.append(threading.Thread(target = Bubblesort, args = (m2list[i],i)))
+    m2thread[i].start()
 
+for i in range(len(m2thread)):
+    m2thread[i].join()
+    
+end = time.time()
 
+print("M2執行時間：%f 秒" % (end - start))
+
+#for i in range(len(m2list)):
+#    print(m2list[i])
+
+#====================================Mission2====================================
