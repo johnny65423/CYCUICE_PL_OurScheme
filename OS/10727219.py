22d57abb413b6663a95ce0a07256feba06ebbbce
johnny65423 <johnny65423@gmail.com>
2021-04-27 19:27:16 +0800

    >

--- OS/10727219.py
+++ OS/10727219.py
@@ -1,22 +1,23 @@
 import time
 import threading
+import multiprocessing as mp
 from numba import jit
 @jit()
 
 
 
-def Bubblesort(data, num):
+def Bubblesort(data):
     # 定義資料長度
     n = len(data)
     xxx = n / 1000
     count = 0
     for i in range(n-1):                   # 有 n 個資料長度，但只要執行 n-1 次
         if( i % xxx == 0 ):
-            #print( count,"%" )
             count+= 1
         for j in range(n-i-1):             # 從第1個開始比較直到最後一個還沒到最終位置的數字 
             if data[j] > data[j+1]:        # 比大小然後互換
                 data[j], data[j+1] = data[j+1], data[j]
+    return data           
     
 def Merge(list1, list2):
     # 定義資料長度
@@ -76,131 +77,144 @@ def Makelist( numlist, k ):
     
     return relist        
 
-#filename = input("請輸入檔案名稱：")
-k = input("請輸入要切成幾份：")
-k = int(k)
-filename = "input_1w"
-openname = filename + ".txt"
-f = open(openname, "r")
 
-numlist = []
-s = 0 
-num = f.readline()
-while num != '':
-    numlist.append(int(num))
-    #s += int(num)
-    #print(int(num))
+if __name__ == "__main__" : 
+    #filename = input("請輸入檔案名稱：")
+    k = input("請輸入要切成幾份：")
+    k = int(k)
+    filename = "input"
+    openname = filename + ".txt"
+    f = open(openname, "r")
+
+    numlist = []
+    s = 0 
     num = f.readline()
-f.close()
+    while num != '':
+        numlist.append(int(num))
+        #s += int(num)
+        #print(int(num))
+        num = f.readline()
+    f.close()
 
 #====================================Mission1====================================
 
-'''
-m1list = numlist.copy()
+    '''
+    m1list = numlist.copy()
 
-start = time.time()
+    start = time.time()
 
-Bubblesort(m1list,0)
+    Bubblesort(m1list)
 
-end = time.time()
+    end = time.time()
 
-m1name = filename+"_output1.txt"
-f = open( m1name, "w" )
+    m1name = filename+"_output1.txt"
+    f = open( m1name, "w" )
 
-f.writelines("Sort : \n")
+    f.writelines("Sort : \n")
 
-for i in range(len(m1list)):
-    f.writelines(str(m1list[i]))
+    for i in range(len(m1list)):
+        f.writelines(str(m1list[i]))
+        f.writelines("\n")
+    print("M1執行時間：%f 秒" % (end - start))
+    f.writelines("CPU Time：%f\n" % (end - start))
+    f.writelines("Output Time：")
+    f.writelines(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) ))
+
     f.writelines("\n")
-print("M1執行時間：%f 秒" % (end - start))
-f.writelines("CPU Time：%f\n" % (end - start))
-f.writelines("Output Time：")
-f.writelines(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) ))
-
-f.writelines("\n")
-f.close()
+    f.close()
 
 #====================================Mission1====================================
 
 #====================================Mission2====================================
     
-start = time.time()    
-  
-m2list = Makelist( numlist, k )
-m2thread = []  
-  
-for i in range(len(m2list)):
-    #print("list", i, ":", m2list[i])
-    m2thread.append(threading.Thread(target = Bubblesort, args = (m2list[i],i)))
-    m2thread[i].start()
+    start = time.time()    
+      
+    m2list = Makelist( numlist, k )
+    m2thread = []  
+     
+    for i in range(len(m2list)):
+        #print("list", i, ":", m2list[i])
+        m2thread.append(threading.Thread(target = Bubblesort, args = (m2list[i])))
+        m2thread[i].start()
+        
 
-for i in range(len(m2thread)):
-    m2thread[i].join()
+    for i in range(len(m2thread)):
+        m2thread[i].join()
 
-while( len(m2list) > 1 ):
-    m2thread2 = []
-    for i in range( int((len(m2list) + 1) / 2 ) ):
-        m2thread2.append(threading.Thread(target = Mergelist, args = (m2list,i, len(m2list) - i - 1)))
+    while( len(m2list) > 1 ):
+        m2thread2 = []
+        for i in range( int((len(m2list) + 1) / 2 ) ):
+            m2thread2.append(threading.Thread(target = Mergelist, args = (m2list,i, len(m2list) - i - 1)))
 
-    for i in range(len(m2thread2)):
-        m2thread2[i].start()
-    for i in range(len(m2thread2)):
-        m2thread2[i].join()
+        for i in range(len(m2thread2)):
+            m2thread2[i].start()
+        #print(m2list)    
+            
+        for i in range(len(m2thread2)):
+            m2thread2[i].join()
+          
+        
+        num = int(len(m2list) - 1) 
+        endindex = int(((len(m2list) + 1) / 2) - 1 )
+        
+        while( num > endindex ) :
+            del m2list[num]
+            num -= 1
+        
+    end = time.time()
 
-    num = int(len(m2list) - 1) 
-    endindex = int(((len(m2list) + 1) / 2) - 1 )
-    
-    while( num > endindex ) :
-        del m2list[num]
-        num -= 1
-    
-end = time.time()
 
+    m2name = filename+"_output2.txt"
+    f = open( m2name, "w" )
 
-m2name = filename+"_output2.txt"
-f = open( m2name, "w" )
+    f.writelines("Sort : \n")
 
-f.writelines("Sort : \n")
+    for i in range(len(m2list[0])):
+        f.writelines(str(m2list[0][i]))
+        f.writelines("\n")
+    print("M2執行時間：%f 秒" % (end - start))
+    f.writelines("CPU Time：%f\n" % (end - start))
+    f.writelines("Output Time：")
+    f.writelines(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) ))
 
-for i in range(len(m2list[0])):
-    f.writelines(str(m2list[0][i]))
     f.writelines("\n")
-print("M2執行時間：%f 秒" % (end - start))
-f.writelines("CPU Time：%f\n" % (end - start))
-f.writelines("Output Time：")
-f.writelines(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) ))
+    f.close()
 
-f.writelines("\n")
-f.close()
-
-'''
+    ''' 
 #====================================Mission2====================================
 
 #====================================Mission3====================================
     
-start = time.time()    
-  
-m3list = Makelist( numlist, k )
+    start = time.time()    
+      
+    m3list = Makelist( numlist, k )
+    print( m3list )
 
-    
-end = time.time()
+      # 設定處理程序數量
+    pool = mp.Pool(k)
+
+      # 運行多處理程序
+    resultlist = pool.map(Bubblesort, m3list)
+
+    print( resultlist )    
+    end = time.time()
 
 
-m3name = filename+"_output3.txt"
-f = open( m3name, "w" )
+    m3name = filename+"_output3.txt"
+    f = open( m3name, "w" )
 
-f.writelines("Sort : \n")
+    f.writelines("Sort : \n")
 
-for i in range(len(m3list[0])):
-    f.writelines(str(m3list[0][i]))
+    for i in range(len(m3list[0])):
+        f.writelines(str(m3list[0][i]))
+        f.writelines("\n")
+    print("M3執行時間：%f 秒" % (end - start))
+    f.writelines("CPU Time：%f\n" % (end - start))
+    f.writelines("Output Time：")
+    f.writelines(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) ))
+
     f.writelines("\n")
-print("M3執行時間：%f 秒" % (end - start))
-f.writelines("CPU Time：%f\n" % (end - start))
-f.writelines("Output Time：")
-f.writelines(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) ))
-
-f.writelines("\n")
-f.close()
+    f.close()
 
 
 #====================================Mission3====================================
--- OS/test.py
+++ OS/test.py
@@ -1,49 +1,22 @@
-import threading
+import multiprocessing as mp
 import time
 
-def print111():
-    print('111')
 
-def Merge(list1, list2):
-    # 定義資料長度
-    i1 = 0
-    i2 = 0
+def mmm(list1) :
+    list1[0] = list1[0] + list1[1]
+    print(list1[0])
+    return list1
+    q.put(list1)
+
+if __name__ == '__main__':
+    m3list = [[1,2], [3,4], [5,6], [7,8] ]
     relist = []
-    while ( i1 < len(list1) - 1 or i2 < len(list2) ) :
-        if( i1 == len(list1) ) :
-            relist.append(list2[i2])
-            i2+= 1
-        elif ( i2 == len(list2) ) :
-            relist.append(list1[i1])
-            i1+= 1
-        else :
-            if( list1[i1] <= list2[i2] ) :
-                relist.append(list1[i1])
-                i1+= 1
-            else :    
-                relist.append(list2[i2])
-                i2+= 1
-    
-    print("done")    
-    return relist
+    print( m3list )
+    q = mp.Queue()
+      # 設定處理程序數量
+    pool = mp.Pool(4)
 
-
-def Mergelist( arraylist, num1, num2 ):
-    if( num1 != num2 ) :
-        arraylist[num1] = Merge( arraylist[num1], arraylist[num2] )
-        del arraylist[num2]
-
-
-a = []
-a.append( [4,6,8] )
-a.append( [7,10] )
-print(a)
-
-
-m2thread2 = threading.Thread(target = Mergelist, args = (a,0, 1))
-
-m2thread2.start()
-
-m2thread2.join() 
-
-print(a)
+      # 運行多處理程序
+    relist = pool.map(mmm, m3list)
+    #relist.append(q.get())
+    print( relist )
