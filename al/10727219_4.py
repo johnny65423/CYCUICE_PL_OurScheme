55929659332eff917f3625c60ade8b47c5a34052
johnny65423 <53753859+johnny65423@users.noreply.github.com>
2021-06-05 21:09:59 +0800

    >

--- al/10727219_4.py
+++ al/10727219_4.py
@@ -1,42 +1,41 @@
+INFINITY = 1e30
 
-def minDistance(num, dist, sptSet):
-    min = 99999999
-    min_index = 0
-    for i in range(num):
-        if dist[i] < min and sptSet[i] == False:
-            min = dist[i]
-            min_index = i
+def Dijkstra(v, source, graph):
+    d = [1e30 for i in range(v + 1)]
+    d[source] = 0
+    set = [True] * (v + 1)
 
-    return min_index
+    n = v
+    while (n != 0):
+        # Extract Minimum
+        u = 0
+        min = INFINITY
+        for i in range(1, v + 1):
+            if (set[i] and d[i] < min):
+                u = i
+                min = d[i]
+        set[u] = False
+        n -= 1
 
-def dijkstra(num, src, graph):
+        # Relax
+        for v in range(1, v + 1):
+            if (graph[u][v] != 0):
+                if (d[v] > d[u] + graph[u][v]):
+                    d[v] = d[u] + graph[u][v]
 
-    dist = [99999999] * num
-    dist[src] = 0
-    sptSet = [False] * num
-
-    for i in range(num):
-
-        u = minDistance(num, dist, sptSet)
-
-        sptSet[u] = True
-
-        for v in range(num):
-            if graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + graph[u][v]:
-                dist[v] = dist[u] + graph[u][v]
-
-    return dist
-
+    for i in range(v):
+        if source != i and d[i] != INFINITY:
+            print(source + 1, 'to', i + 1, '=', d[i])
 
 
 num = int(input())
 
 while num != 0 :
     lines = int(input())
-    v = int(input())
+    source = int(input())
 
-    g = [[0 for i in range(num)]
-            for j in range(num)]
+    g = [[0 for i in range(num+1)]
+            for j in range(num+1)]
     for i in range(lines) :
         info = input()
         info = info.split()
@@ -45,13 +44,7 @@ while num != 0 :
             info[j] = int(info[j])
         g[info[0]-1][info[1]-1] = info[2]
 
-
-
-    ans = []
-    ans = dijkstra(num, v - 1, g)
-
-    for i in range(1, len(ans)) :
-        print('1 to', i+1, '=', ans[i])
+    Dijkstra(num, source-1, g)
 
     num = int(input())
 
